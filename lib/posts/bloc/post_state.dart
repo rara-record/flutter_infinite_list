4217b4ab09935aab538559774d670cd9b69ab216
BORA <a52447879@gmail.com>
2024-09-02 21:43:47 +0900

    feat: Add PostState class to define states for PostBloc

---
+++ lib/posts/bloc/post_state.dart
@@ -0,0 +1,42 @@
+part of 'post_bloc.dart';
+
+/// =============
+/// initial: 초기 상태
+/// success :
+///   - posts: 현재까지 검색된 게시물 목록을 나타내는 List<Post>가 표시됩니다.
+///   - hasReachedMax: 최대 게시물 수에 도달했는지 여부를 알려주는 List<Post>가 표시될 것입니다.
+/// failure : 게시물을 가져오는 동안 오류가 발생
+
+enum PostStatus { initial, success, failure }
+
+final class PostState extends Equatable {
+  const PostState({
+    this.status = PostStatus.initial,
+    this.posts = const <Post>[],
+    this.hasReachedMax = false,
+  });
+
+  final PostStatus status;
+  final List<Post> posts;
+  final bool hasReachedMax;
+
+  PostState copyWith({
+    PostStatus? status,
+    List<Post>? posts,
+    bool? hasReachedMax,
+  }) {
+    return PostState(
+      status: status ?? this.status,
+      posts: posts ?? this.posts,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''PostState { status: $status, hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, posts, hasReachedMax];
+}
