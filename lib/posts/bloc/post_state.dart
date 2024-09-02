04977fcb2e57deb10dc47c5613beba0569d3dabf
BORA <a52447879@gmail.com>
2024-09-02 22:15:58 +0900

    feat: Implement throttling for fetching posts in PostBloc

--- lib/posts/bloc/post_state.dart
+++ lib/posts/bloc/post_state.dart
@@ -1,13 +1,14 @@
 part of 'post_bloc.dart';
 
-/// =============
 /// initial: 초기 상태
-/// success :
-///   - posts: 현재까지 검색된 게시물 목록을 나타내는 List<Post>가 표시됩니다.
-///   - hasReachedMax: 최대 게시물 수에 도달했는지 여부를 알려주는 List<Post>가 표시될 것입니다.
+/// success : 성공
 /// failure : 게시물을 가져오는 동안 오류가 발생
 
 enum PostStatus { initial, success, failure }
+
+/// posts: 현재까지 검색된 게시물 목록을 나타내는 List<Post>가 표시됩니다.
+///
+/// hasReachedMax: 최대 게시물 수에 도달했는지 여부를 알려주는 List<Post>가 표시될 것입니다.
 
 final class PostState extends Equatable {
   const PostState({
